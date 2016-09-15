5099be12f3c5d77a9a9d9428403f9eb7ede11d2b
Nick <Nick>
2016-09-15 17:10:28 -0400

    Added debug mode.

--- dxf2scr.py
+++ dxf2scr.py
@@ -8,22 +8,28 @@ file = open(outputFilename, 'w')
 
 dwg = ezdxf.readfile(str(sys.argv[1]))
 
+'''Debug mode prints all output if equal to 1'''
+debugMode = 0
+
 ''' Will add a switch flag for INCH or MM later '''
 gridOutput = 'GRID {};\n'.format('INCH')
-print gridOutput,
 file.write(gridOutput)
+if debugMode == 1:
+    print gridOutput,
 
 ''' Layer 20 is the dimension layer in Eagle '''
 layerOutput = 'LAYER {};\n'.format('20')
-print layerOutput,
 file.write(layerOutput)
+if debugMode == 1:
+    print layerOutput,
 
 modelspace = dwg.modelspace()
 
 def makeLine(e):
     lineOutput = ('WIRE ({} {}) ({} {})\n'.format(str(e.dxf.start[0]), str(e.dxf.start[1]), str(e.dxf.end[0]), str(e.dxf.end[1])))
-    print lineOutput,
     file.write(lineOutput)
+    if debugMode == 1:
+        print lineOutput,
     '''
     Eagle defines a a WIRE with a start and end point.
     WIRE  (Start Point) (End Point)
@@ -31,8 +37,9 @@ def makeLine(e):
     
 def makeCircle(e):
     circleOutput = ('CIRCLE ({} {}) ({} {})\n'.format(str(e.dxf.center[0]), str(e.dxf.center[1]), str(e.dxf.radius+e.dxf.center[0]), str(e.dxf.center[1])))
-    print circleOutput,
     file.write(circleOutput)
+    if debugMode == 1:
+        print circleOutput,
     '''
     Eagle defines a circle with a center point and a point anywhere
     on the circumference.
@@ -47,8 +54,9 @@ def makeArc(e):
     arcEndX = round(e.dxf.center[0] + (e.dxf.radius * math.cos(math.radians(e.dxf.end_angle))), 3) + 0
     arcEndY = round(e.dxf.center[1] + (e.dxf.radius * math.sin(math.radians(e.dxf.end_angle))), 3) + 0
     arcOutput = ( ('ARC CCW ({} {}) ({} {}) ({} {})\n').format(str(arcStartX), str(arcStartY), str(arcOppX), str(arcOppY), str(arcEndX), str(arcEndY)) )
-    print arcOutput,
     file.write(arcOutput)
+    if debugMode == 1:
+        print arcOutput,
     '''
     Eagle defines an arc with three points. The first point is the 
     start of the arc. The second point is opposite the start point
@@ -66,8 +74,9 @@ def makePoly(e):
         polyEndY = round(list(e.points())[i][1], 3)
         if polyStartX != polyEndX or polyStartY != polyEndY:
             polyOutput = ('WIRE ({} {}) ({} {})\n').format(polyStartX, polyStartY, polyEndX, polyEndY)
-            print polyOutput,
             file.write(polyOutput)
+            if debugMode == 1:
+                print polyOutput,
     '''
     ezdxf renderes ellipses as polylines. The polyline is broken up
     into it's individual points and lines are drawn from point to 
